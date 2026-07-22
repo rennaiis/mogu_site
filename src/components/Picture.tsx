daef75bf5558d83a239249ae36aa17cde952b6e0
rennaiis <isaeva.nastya04@gmail.com>
2026-07-22 12:35:45 +0300

    feat: сделан компонент picture

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -1,7 +1,27 @@
-function Picture(){
+import type React from "react"
+
+interface sourceItem {
+    media: string,
+    srcset: string
+}
+
+interface pictureProps{
+    sources: sourceItem[], 
+    imgAttrs: React.ImgHTMLAttributes<HTMLImageElement>
+}
+
+function Picture({sources, imgAttrs}: pictureProps){
     return (
-        <>
-        </>
+        <picture>
+            {sources && sources.map((source, index)=>(
+                <source 
+                    key={index}
+                    media={source.media}
+                    srcSet={source.srcset}
+                />
+            ))}
+            {imgAttrs && <img {...imgAttrs} />}
+        </picture>
     )
 }
 
